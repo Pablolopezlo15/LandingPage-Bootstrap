3f64bfcf6de1c228058c5c4b72b7569bb3bc7d9f
Pablolopezlo15 <plopezlozano12@gmail.com>
2024-01-22 18:57:09 +0100

    Landing-Page

---
+++ src/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    var myOffcanvas = document.getElementById('offcanvasTop');
+    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+    bsOffcanvas.show();
+});
