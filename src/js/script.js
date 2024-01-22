5149e58dc2acf5232a512816a34283ade3d3fd39
Pablo Lopez Lozano <plopezlozano12@gmail.com>
2024-01-22 21:13:58 +0100

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    var myOffcanvas = document.getElementById('offcanvasTop');
-    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+    var exampleModal = document.getElementById('exampleModal');
+    var bsOffcanvas = new bootstrap.Offcanvas(exampleModal);
     bsOffcanvas.show();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Muestra el modal automáticamente
+    var myModal = new bootstrap.Modal(document.getElementById('cookieModal'), {
+        backdrop: 'static', // No se cierra al hacer clic fuera del modal
+        keyboard: false // No se cierra con la tecla Esc
+    });
+    myModal.show();
 });
